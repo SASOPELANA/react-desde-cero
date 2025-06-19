93d06a84d95bc86b83b73a8b4bc44653469b99dd
Sergio <sopekof@gmail.com>
2025-06-19 17:18:49 -0300

    Curso de React desde cero.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<header>
+				<h1> Hola desde React y Vite, curso desde cero!! </h1>
+			</header>
+
+			<section>
+				<p> Esto es un parrafo </p>
+			</section>
+
+			<footer>
+				<p> Esto es un pie de pagina </p>
+			</footer>
+		</>
+	);
+}
+
+export default App;
